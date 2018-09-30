e9b9b5a0fc25b4fc8654bd8e672e31dce34c41d1
wutsol <1831860861@qq.com>
2018-09-30 10:31:08 +0800

    增加表达式求值的应用

--- sqstack.cpp
+++ sqstack.cpp
@@ -5,7 +5,21 @@ using namespace std;
 int main()
 {
 	SqStack<int> intSqStack;
-	intSqStack.conversion(159, 8);
+	//cout<<intSqStack.conversion(159, 8)<<endl;
+	cout << "请输入前缀表达式：（以#结束）" << endl;
+	char ptr[10],*exp;
+	int i = 0,j=0;
+	do { cin >> ptr[i]; } while (ptr[i++] != '#');
+	cout << "后缀表达式为：";
+	exp = intSqStack.change(ptr);
+	while (*(exp+j) != '\0')
+	{
+		cout << *(exp + j);
+		j++;
+	}
+	cout << "表达式结果为：";
+	cout << intSqStack.EvalExpr_RPN(exp);
+	//cout << intSqStack.EvalExpr(ptr);
 	system("pause");
 	return 0;
 }
